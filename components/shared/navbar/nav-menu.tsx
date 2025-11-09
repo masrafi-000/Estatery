1c58f9c4b37792f431c7c02c02dbbe2f8f096ebc
masrafi-000 <smmasrafi000@gmail.com>
2025-11-09 19:06:39 +0600

    Update global styles in CSS for improved typography and color scheme; modify navbar menu styles for enhanced visual appeal.

--- components/shared/navbar/nav-menu.tsx
+++ components/shared/navbar/nav-menu.tsx
@@ -27,7 +27,7 @@ const NavMenu = ({ openNav }: Props) => {
           className="flex items-center gap-2"
         >
           <div className="flex h-10 w-10 justify-center items-center rounded-lg bg-primary">
-            <span className="text-blue-500 text-lg font-bold ">E</span>
+            <span className="text-pink-800 text-xl font-bold ">E</span>
           </div>
           <span className="text-xl font-semibold ">Estatery</span>
         </Link>
